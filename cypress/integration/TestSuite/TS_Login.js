bf2d532dc22b8e663fb0b797919f3710e96204a8
LamHo <lam.ho@ptnglobalcorp.com>
2021-12-09 10:37:04 +0700

    test

--- cypress/integration/TestSuite/TS_Login.js
+++ cypress/integration/TestSuite/TS_Login.js
@@ -11,7 +11,8 @@ describe("Test Shopping Login Page", () => {
     );
   });
 
-  it("Verify Shopping Login success with valid userID and valid password", function () {
+  it("Verify Shopping Login success with valid email and valid password", function () {
+    //input valid email and password
     const login = new LoginPage();
     login.inputEmail(this.testLogin[0].email);
     login.inputPassword(this.testLogin[0].password);
@@ -19,5 +20,81 @@ describe("Test Shopping Login Page", () => {
     login.msgLoginSussess();
   });
 
+  it("Verify Shopping Login fail with invalid email and invalid password", function () {
+    //input invalid email and invalid password
+    const login = new LoginPage();
+    login.inputEmail(this.testLogin[1].email);
+    login.inputPassword(this.testLogin[1].password);
+    login.buttonSignIn();
+    login.msgError3();
+  });
 
+  it("Verify Shopping Login fail with blank data", function () {
+    //input blank email and blank password
+    const login = new LoginPage();
+    cy.get('input[id="email"]').should("have.value", '');
+    cy.get('input[id="passwd"]').should("have.value", '');
+    login.buttonSignIn();
+    login.msgError1();
+  });
+  it("Verify Shopping Login fail with not format email", function () {
+    //input not format email
+    const login = new LoginPage();
+    login.inputEmail(this.testLogin[2].email);
+    login.inputPassword(this.testLogin[0].password);
+    login.buttonSignIn();
+    login.msgError2();
+  });
+
+  it("Verify Shopping Login fail with input number email", function () {
+    //input number email field
+    const login = new LoginPage();
+    login.inputEmail(this.testLogin[3].email);
+    login.inputPassword(this.testLogin[0].password);
+    login.buttonSignIn();
+    login.msgError2();
+  });
+
+  it("Verify Shopping Login fail with input space email", function () {
+    //input space email field
+    const login = new LoginPage();
+    login.inputEmail(this.testLogin[4].email);
+    login.inputPassword(this.testLogin[0].password);
+    login.buttonSignIn();
+    login.msgError2();
+  });
+
+  it("Verify user retrieved password success with input valid email", function () {
+    //user forgot and retrive password with valid email
+    const login = new LoginPage();
+    login.inputEmail(this.testLogin[0].email);
+    login.clickForgotPassword();
+    login.inputEmail(this.testLogin[0].email);
+    login.buttonRetrivePass();
+    login.msgRetrievePassSuccess();
+    login.msgRetrievePassSuccess();
+    cy.should(
+      "have.text",
+      "A confirmation email has been sent to your address: " +
+        this.testLogin[0].email
+    );
+  });
+
+  it("Verify user retrieved password fail with input invalid email", function () {
+    //user forgot and retrive password fail with invalid email
+    const login = new LoginPage();
+    login.clickForgotPassword();
+    login.inputEmail(this.testLogin[1].email);
+    login.buttonRetrivePass();
+    login.msgError4();
+  });
+
+  it("Verify user retrieved password fail with blank email", function () {
+    //user forgot and retrive password fail with blank email
+    const login = new LoginPage();
+    login.clickForgotPassword();
+    cy.get('input[id="email"]').should("have.value", "");
+    login.buttonRetrivePass();
+    login.msgError2();
+  });
 });
--- cypress/support/PageObject/LoginPage.js
+++ cypress/support/PageObject/LoginPage.js
@@ -1,22 +1,69 @@
-class LoginPage
-{
-    inputEmail(email){
-        cy.get('input[id="email"]').clear().type(email);
-        return this
-    }
+class LoginPage {
+  inputEmail(email) {
+    //get email to email address field
+    cy.get('input[id="email"]').clear().type(email);
+    return this;
+  }
 
-    inputPassword(password){
-        cy.get('input[id="passwd"]').clear().type(password);
-        return this
-    }
+  inputPassword(password) {
+    //get password to password field
+    cy.get('input[id="passwd"]').clear().type(password);
+    return this;
+  }
 
-    buttonSignIn(){
-        cy.get('i[class="icon-lock left"]').click();
-    }
-    msgLoginSussess(){
-        cy.get('p[class="info-account"]').should(
-          "have.text",
-          "Welcome to your account. Here you can manage all of your personal information and orders.".trim());
-    }
+  buttonSignIn() {
+    //click button Sing in
+    cy.get('i[class="icon-lock left"]').click();
+  }
+  msgLoginSussess() {
+    //show message when login successful
+    cy.get('p[class="info-account"]').should(
+      "have.text",
+      "Welcome to your account. Here you can manage all of your personal information and orders.".trim()
+    );
+  }
+
+  msgError1() {
+    //show message when blank email and password
+    cy.xpath('//li[text()="An email address required."]').should(
+      "have.text","An email address required.");
+  }
+
+  msgError2() {
+    //show message when input not format email
+    cy.xpath('//li[text()="Invalid email address."]').should(
+      "have.text", "Invalid email address."
+    );
+  }
+
+  msgError3() {
+    //show message when input invalid email and invalid password
+    cy.xpath('//li[text()="Authentication failed."]').should(
+      "have.text", "Authentication failed.");
+  }
+
+  clickForgotPassword() {
+      //user click forgot password
+      cy.get("a[href='http://automationpractice.com/index.php?controller=password']").click();
+  }
+
+  buttonRetrivePass(){
+    //click button retrive password
+    cy.xpath("//span[text()='Retrieve Password']").click();
+  }
+
+  msgRetrievePassSuccess(){ 
+    //show message when retrive password is successful 
+    cy.get("p[class='alert alert-success']");
+  }
+
+  msgError4(){
+    //show message when input invalid email
+    cy.xpath("//div[@class='alert alert-danger']").contains("There is no account registered for this email address.");
+  }
+  clickSignOut(){
+    //ckick log out account
+    cy.get('a[class="logout"]').click();
+  }
 }
 export default LoginPage;
